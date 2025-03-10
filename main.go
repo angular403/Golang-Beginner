85debabcf0ef72b8c1f800be42ae7164eb9ff615
Andrew Napitupulu <angularsprings403@gmail.com>
2025-03-10 11:09:16 +0700

    Deklarasi Variabel Golang

--- main.go
+++ main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	number1 := 123
+	fmt.Println(number1)
+
+	number1 = 132
+	fmt.Println(number1)
+
+	var floatNumber float64 = -1234121.01
+	floatNumber2 := 123.0
+	fmt.Println(floatNumber)
+	fmt.Println(floatNumber2)
+
+
+	var name string = "andrew"
+	name2 := "wiliam"
+
+	fmt.Println(name)
+	fmt.Println(name2)
+
+
+	var yes bool = true
+	var no bool = false
+
+	fmt.Println(yes)
+	fmt.Println(no)
 }
